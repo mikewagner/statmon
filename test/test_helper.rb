dc0dd7f79a78116d73c04952dde448627a9ee3ac
Michael Wagner <mwagner@digitalinsites.com>
2010-09-16 12:25:12 -0500

    removed test files

--- test/test_helper.rb
+++
@@ -1,7 +0,0 @@
-require 'rubygems'
-
-gem 'shoulda',           '~> 2.11.3'
-
-$:.unshift File.expand_path(File.dirname(__FILE__) + '/../lib')
-require 'stat_mon'
-require 'shoulda'
--- test/unit/test_stat_mon.rb
+++
@@ -1,8 +0,0 @@
-require 'test_helper.rb'
-
-class StatMonTest < Test::Unit::TestCase
-  should "be able to start listener" do
-    listener = StatMon::
-    MongoMapper.connection = conn
-    MongoMapper.connection.should == conn
-  end
